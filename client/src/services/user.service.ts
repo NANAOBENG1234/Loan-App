92aba1ccc9aa6c2e0d0431210b7d73df7f5eae58
Nana_Obeng <bnoagyemang@st.ug.edu.gh>
2026-09-11 04:12:55 +0000

    feat(client): add changePassword and deleteAccount to user service

--- client/src/services/user.service.ts
+++ client/src/services/user.service.ts
@@ -6,9 +6,24 @@ export interface UpdateProfileInput {
   email?: string | null;
 }
 
+export interface ChangePasswordInput {
+  currentPassword: string;
+  newPassword: string;
+}
+
 export const userService = {
   async updateProfile(data: UpdateProfileInput) {
     const res = await api.put("/users/profile", data);
+    return res.data;
+  },
+
+  async changePassword(data: ChangePasswordInput) {
+    const res = await api.put("/users/password", data);
+    return res.data;
+  },
+
+  async deleteAccount(password: string) {
+    const res = await api.delete("/users/account", { data: { password } });
     return res.data;
   },
 };
