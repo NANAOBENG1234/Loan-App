8c1fc99f0ff97f90e5a62b315848249d7536a7e5
Nana_Obeng <bnoagyemang@st.ug.edu.gh>
2026-09-12 01:22:40 +0000

    chore(client): remove unused components

--- client/src/components/layout/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export function Footer() {
-  return (
-    <footer className="bg-white border-t border-gray-200 py-4 text-center text-sm text-gray-500">
-      &copy; {new Date().getFullYear()} LoanApp. All rights reserved.
-    </footer>
-  );
-}
